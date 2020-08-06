051958e4dd63e6fa76b642e96697184795d0c5f0
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-08-06 17:45:45 -0400

    Trying HiFi testing with audio.

--- cypress/integration/webrtc.js
+++ cypress/integration/webrtc.js
@@ -1,4 +1,8 @@
-it('uses webrtc', function() {
-  cy.visit('https://meet.jit.si/cypress-audio-test#config.prejoinPageEnabled=false')
+it('log into HiFi space as DJ', function() {
+  cy.visit('/DfFq8zih057Z3qe3/?x=38.63&y=24.56&o=4.71')
+  cy.get('.screen1__play').click()
+  cy.get('.allowScreen__askForPermissionButton *').click()
+  cy.get('.chooseIOScreen__nextButton').click()
+  cy.get('.profileScreen__readyButton').click()
   cy.wait(20000)
 })

7e88b0056283fd21a80308451b74ac97062e6c5e
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-08-06 13:34:47 -0400

    Added webrtc test.

---
+++ cypress/integration/webrtc.js
@@ -0,0 +1,6 @@
+it('uses webrtc', function() {
+  cy.visit('https://appr.tc/?debug=loopback')
+  cy.get('input#room-id-input').type('625438846')
+  cy.get('button#join-button').click()
+  cy.wait(10000)
+)}

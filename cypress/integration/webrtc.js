2d02829fd05c4c99268725239a2bbd9a359a6aae
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-08-06 17:50:46 -0400

    Only click first.

--- cypress/integration/webrtc.js
+++ cypress/integration/webrtc.js
@@ -1,7 +1,7 @@
 it('log into HiFi space as DJ', function() {
   cy.visit('/DfFq8zih057Z3qe3/?x=38.63&y=24.56&o=4.71')
   cy.get('.screen1__play').click()
-  cy.get('.allowScreen__askForPermissionButton *').click()
+  cy.get('.allowScreen__askForPermissionButton *').first().click()
   cy.get('.chooseIOScreen__nextButton').click()
   cy.get('.profileScreen__readyButton').click()
   cy.wait(20000)

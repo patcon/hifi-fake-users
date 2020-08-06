c5c52d5ad9f35442b24216b2564d6d4816c9bead
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-08-06 13:53:47 -0400

    Skip the button to join jitsi room.

--- cypress/integration/webrtc.js
+++ cypress/integration/webrtc.js
@@ -1,5 +1,4 @@
 it('uses webrtc', function() {
-  cy.visit('https://meet.jit.si/625438846')
-  cy.contains('div', 'Join meeting').click()
+  cy.visit('https://meet.jit.si/625438846#config.prejoinPageEnabled=false')
   cy.wait(10000)
 })

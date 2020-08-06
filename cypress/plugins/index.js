a94b1f7d8e17d1e8819829b8f4c2db6ff5d3cf84
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-08-06 13:31:03 -0400

    Added mock mic.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,6 +8,11 @@ module.exports = on => {
       // https://www.ghacks.net/2013/10/06/list-useful-google-chrome-command-line-switches/
       launchOptions.args.push('--window-size=1920,1080')
 
+      // See: https://github.com/cypress-io/cypress/issues/5592
+      launchOptions.args.push('--use-fake-device-for-media-stream')
+      launchOptions.args.push('--use-fake-ui-for-media-stream')
+      launchOptions.args.push('--use-file-for-fake-audio-capture=cypress/fixtures/female.wav')
+
       console.log('chrome launch args:')
       console.log(launchOptions.args.join(os.EOL))
       return launchOptions
